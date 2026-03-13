a273c7b5867e7b96e6321447283cda5b9678d9f6
Allen Lin <al319966@gmail.com>
2026-03-13 11:05:09 -0400

    seperation confusion :

--- frontend/src/Forum.js
+++ frontend/src/Forum.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react'
 
 function Forum() {
-  const [message, setMessage] = useState('')
+  const [intro, setIntro] = useState('')
 
   useEffect(() => {
     fetch('http://127.0.0.1:8000/api/forum/')
       .then(res => res.json())
-      .then(data => setMessage(data.message))
+      .then(data => setIntro(data.message))
   }, [])
 
-  return <h1>{message || 'Loading...'}</h1>
+  return <h1>{intro || 'Loading...'}</h1>
 }
 
 export default Forum
